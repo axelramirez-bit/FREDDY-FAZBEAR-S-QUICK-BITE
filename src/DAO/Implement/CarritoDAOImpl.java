a6d996c5add7c22b7f2a756c0779d7eafa5c7855
Melany <melanymejia@emilianisomascos.edu.gt>
2026-07-16 10:19:26 -0600

    Añadir DAOS e IMPLEMENTS

--- src/DAO/Implement/CarritoDAOImpl.java
+++ src/DAO/Implement/CarritoDAOImpl.java
@@ -1,13 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package DAO.Implement;
 
-/**
- *
- * @author Computacion
- */
-public class CarritoDAOImpl {
+import Config.Conexion;
+import DAO.Interfaz.ICarritoDAO;
+import Model.Carrito;
+import Model.CarritoDetalle;
+import Model.EstadoCarrito;
+import java.sql.Timestamp;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+
+public class CarritoDAOImpl implements ICarritoDAO {
+
+    @Override
+public boolean crear(Carrito carrito) {
+
+    String sql = "INSERT INTO carrito(id_usuario, fecha_creacion, estado) VALUES (?, ?, ?)";
+
+    try (Connection con = Conexion.getInstancia().getConexion();
+         PreparedStatement ps = con.prepareStatement(sql)) {
+
+        ps.setInt(1, carrito.getUsuario().getIdUsuario());
+        ps.setTimestamp(2, Timestamp.valueOf(carrito.getFechaCreacion()));
+        ps.setString(3, carrito.getEstado().name());
+
+        return ps.executeUpdate() > 0;
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return false;
+    }
+
+
+}
+
+    @Override
+    public Carrito buscarPorId(int idCarrito) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Carrito buscarPorUsuario(int idUsuario) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean actualizarEstado(int idCarrito, EstadoCarrito estado) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean vaciarCarrito(int idCarrito) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<CarritoDetalle> obtenerDetalles(int idCarrito) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
--- src/DAO/Implement/CategoriaDAOImpl.java
+++ src/DAO/Implement/CategoriaDAOImpl.java
@@ -1,14 +1,62 @@
 package DAO.Implement;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import Config.Conexion;
+import DAO.Interfaz.ICategoriaDAO;
+import Model.Categoria;
 
-/**
- *
- * @author Computacion
- */
-public class CategoriaDAOImpl {
-    
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class CategoriaDAOImpl implements ICategoriaDAO {
+
+    @Override
+    public boolean guardar(Categoria categoria) {
+
+        String sql
+                = "INSERT INTO categoria(nombre,descripcion,icono,imagen,estado) VALUES (?,?,?,?,?)";
+
+        try (Connection con = Conexion.getInstancia().getConexion(); PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, categoria.getNombre());
+            ps.setString(2, categoria.getDescripcion());
+            ps.setString(3, categoria.getIcono());
+            ps.setString(4, categoria.getImagen());
+            ps.setBoolean(5, categoria.isEstado());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    @Override
+    public boolean actualizar(Categoria categoria) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean cambiarEstado(int idCategoria, boolean estado) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Categoria buscarPorId(int idCategoria) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Categoria> listar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Categoria> listarActivas() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
